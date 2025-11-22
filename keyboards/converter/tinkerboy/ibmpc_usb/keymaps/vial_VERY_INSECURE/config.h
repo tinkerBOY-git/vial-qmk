548e191b5a598fb982e095921f642c8a7f79301f
tinkerBOY <gio@tinkerboy.xyz>
2025-11-22 11:34:06 +0800

    Add files via upload

---
+++ keyboards/converter/tinkerboy/ibmpc_usb/keymaps/vial_VERY_INSECURE/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x51, 0x73, 0x85, 0x91, 0x1B, 0xC7, 0x66, 0x7B}
+#define VIAL_UNLOCK_COMBO_ROWS { 2, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 9, 8 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
